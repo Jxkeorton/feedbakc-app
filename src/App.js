641ab68c8c70d7fa1eddf31631b92ec00799b0ad
Jxkeorton <jakeorton99@gmail.com>
2023-03-09 10:11:56 +0000

    using routes to create an about page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { v4 as uuidv4 } from 'uuid'
+import { useState } from "react"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header"
 import FeedbackList from "./components/FeedbackList"
-import { useState } from "react"
 import FeedbackData from "./data/FeedbackData"
 import React from 'react'
 import FDeedabackStats from "./components/FDeedabackStats"
 import FeedbackForm from "./components/FeedbackForm"
+import AboutPage from './pages/AboutPage'
+import AboutIconLink from './components/AboutIconLink'
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData)
@@ -22,16 +25,29 @@ function App() {
     }
 
     return (
-        <>
+        <Router>
             <Header />
             <div className='container' >
-                <FeedbackForm handleAdd={addFeedback} />
-                <FDeedabackStats feedback={feedback} />
-                <FeedbackList 
-                feedback={feedback}  
-                handleDelete={deleteFeedback}/>
+                <Routes>
+                    <Route exact path='/' element={
+                        <>
+                            <FeedbackForm handleAdd={addFeedback} />
+                            <FDeedabackStats feedback={feedback} />
+                            <FeedbackList 
+                            feedback={feedback}  
+                            handleDelete={deleteFeedback}/>
+                        </>
+                    }>
+                    </Route> 
+                    
+                    <Route path='/about' element={<AboutPage />}/>
+
+
+                </Routes>
+
+                <AboutIconLink />
             </div>
-        </>
+        </Router>
       
     )
 }
